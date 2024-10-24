582c549b0856a2ccc77aaf9c575ee1ad8f9588a5
AlexKSP <127268092+AlexKSP@users.noreply.github.com>
2024-10-24 20:29:09 -0300

    Criado orderController, e realizado alterações de erros de codigo

--- src/app/schemas/Order.js
+++ src/app/schemas/Order.js
@@ -1,53 +1,53 @@
 import mongoose from "mongoose";
 
-const Orderschema = new mongoose.Schema({
-  user: {
-    id: {
-      type: String,
-      required: true,
-    },
-    name: {
-      type: {
-        type: String,
-        required: true,
-      },
-    },
-  },
-  products: [
-    {
+const Orderschema = new mongoose.Schema(
+  {
+    user: {
       id: {
-        type: Number,
+        type: String,
         required: true,
       },
       name: {
-        type: String,
-        required: true,
-      },
-      price: {
-        type: Number,
-        required: true,
-      },
-      category: {
-        type: String,
-        required: true,
-      },
-      url: {
-        type: String,
-        required: true,
-      },
-      quantity: {
-        type: Number,
-        required: true,
+          type: String,
+          required: true,
       },
     },
-  ],
-  status: {
-    type: String,
-    required: true,
+    products: [
+      {
+        id: {
+          type: Number,
+          required: true,
+        },
+        name: {
+          type: String,
+          required: true,
+        },
+        price: {
+          type: Number,
+          required: true,
+        },
+        category: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+        quantity: {
+          type: Number,
+          required: true,
+        },
+      },
+    ],
+    status: {
+      type: String,
+      required: true,
+    },
   },
   {
     timestamps: true,
   },
-});
+);
 
-export default mongoose.model('Order', Orderschema)
+export default mongoose.model("Order", Orderschema);
