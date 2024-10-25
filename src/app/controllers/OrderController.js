56aa0196bc99cd5079bd15e71f9bb36c5f1e3714
AlexKSP <127268092+AlexKSP@users.noreply.github.com>
2024-10-24 21:08:33 -0300

    Realizado atualização de pedido, adicionado rota put para update de status do pedido

--- src/app/controllers/OrderController.js
+++ src/app/controllers/OrderController.js
@@ -1,20 +1,21 @@
 import * as Yup from "yup";
-import Order from '../schemas/Order'
+import Order from "../schemas/Order";
 import Product from "../models/Product";
 import Category from "../models/Category";
+import { or } from "sequelize";
 
 class OrderController {
   async store(request, response) {
-     const schema = Yup.object({
-       products: Yup.array()
-         .required()
-         .of(
-           Yup.object({
-             id: Yup.number().required(),
-             quantity: Yup.number().required(),
-           })
-         ),
-     });
+    const schema = Yup.object({
+      products: Yup.array()
+        .required()
+        .of(
+          Yup.object({
+            id: Yup.number().required(),
+            quantity: Yup.number().required(),
+          })
+        ),
+    });
 
     try {
       schema.validateSync(request.body, { abortEarly: false });
@@ -27,45 +28,76 @@ class OrderController {
     const productIds = products.map((product) => product.id);
 
     const findProducts = await Product.findAll({
-        where: {
-            id: productIds,
+      where: {
+        id: productIds,
+      },
+      include: [
+        {
+          model: Category,
+          as: "category",
+          attributes: ["name"],
         },
-        include: [
-            {
-                model: Category,
-                as: 'category',
-                attributes: ['name'],
-            },
-        ],
+      ],
     });
 
     const formattedProducts = findProducts.map((product) => {
-        const productIndex = products.findIndex((item) => item.id === product.id);
+      const productIndex = products.findIndex((item) => item.id === product.id);
 
-        const newProduct = {
-            id: product.id,
-            name: product.name,
-            category: product.category.name,
-            price: product.price,
-            url: product.url,
-            quantity: products[productIndex].quantity,
-        };
+      const newProduct = {
+        id: product.id,
+        name: product.name,
+        category: product.category.name,
+        price: product.price,
+        url: product.url,
+        quantity: products[productIndex].quantity,
+      };
 
-        return newProduct;
+      return newProduct;
     });
 
     const order = {
-        user: {
-            id: request.userId,
-            name: request.userName,
-        },
-        products: formattedProducts,
+      user: {
+        id: request.userId,
+        name: request.userName,
+      },
+      products: formattedProducts,
+      status: "Pedido realizado",
     };
-      
-    return response.status(201).json(order);
+
+    const createdOrder = await Order.create(order);
+
+    return response.status(201).json(createdOrder);
   }
 
+  async index(request, response) {
+    const orders = await Order.find();
+
+    return response.json(orders)
+  }
+
+  async update(request, response) {
+    const schema = Yup.object({
+        status: Yup.string().required(),
+      });
   
+      try {
+        schema.validateSync(request.body, { abortEarly: false });
+      } catch (err) {
+        return response.status(400).json({ error: err.errors });
+      }
+
+      const { id } = request.params;
+      const { status } = request.body;
+
+      try {
+        await Order.updateOne({ _id: id}, { status });
+      } catch (err) {
+        return response.status(400).json({ error: err.message });
+      }
+      
+
+      return response.json({ message: 'Status updated sucessfully' });
+  }
 }
 
 export default new OrderController();
